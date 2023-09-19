7b30388d80d7ae0813489ef221f027eb59e827e2
SickGuyy777 <57241415+KAWkkkk@users.noreply.github.com>
2023-09-19 19:10:58 -0300

    cosas

---
+++ Seminario_DamuriRomeroTeves/Assets/Scripts/PickableObj.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObj : MonoBehaviour
+{
+    public float pickRadius;
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+}
