178f807b0e78a135bf1ca73478c6d47f08aa6648
SickGuyy777 <benjaminteves2002@gmail.com>
2023-10-02 17:44:16 -0300

    cosas 2

--- Seminario_DamuriRomeroTeves/Assets/Scripts/Events/EventsManager.cs
+++ Seminario_DamuriRomeroTeves/Assets/Scripts/Events/EventsManager.cs
@@ -7,6 +7,7 @@ public class EventsManager : MonoBehaviour
     [SerializeField] protected TimeSystem _timeSys;
     [SerializeField] GameObject _mafiaBoss;
     [SerializeField] GameObject _tortureSpawnPoint;
+    [SerializeField] GameObject initialSpawnPoint;
 
     public bool tortureEvent;
 
@@ -23,6 +24,12 @@ public class EventsManager : MonoBehaviour
         {
             _mafiaBoss.transform.position = _tortureSpawnPoint.transform.position;
             _mafiaBoss.SetActive(true);
+        }
+        else if(_timeSys.currentHour == 0)
+        {
+            _mafiaBoss.transform.position = initialSpawnPoint.transform.position;
+            tortureEvent = false;
+            _mafiaBoss.SetActive(false);
         }
         else
             _mafiaBoss.SetActive(false);
