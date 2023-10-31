7537a16c1791af20261f4464ecf46c0157afb3f6
romelau26 <92961955+romelau26@users.noreply.github.com>
2023-10-30 23:55:32 -0300

    calendario

--- Seminario_DamuriRomeroTeves/Assets/Scripts/World/TimeSystem.cs
+++ Seminario_DamuriRomeroTeves/Assets/Scripts/World/TimeSystem.cs
@@ -16,20 +16,31 @@ public class TimeSystem : MonoBehaviour
     public TMP_Text monthText;
     public TMP_Text yearText;
     public TMP_Text AmPm;
+    public GameObject[] daysMoth;
     public int multipltime;
     public float currentHour;
     public int currentDay;
-    //public int currentMonth;
+    public int currentMonth;
     //public int currentYear;
 
     public float CurrentMinutes;
     private float seconds;
     string[] _daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    string[] _MonthOfYear = { "January", "February", "March", "April", "May", "June,", "July" , "August" , "September" , "October" , "November" , "December" }; //esto va a salir en el calendario
     public string currentDayOfWeek = "Monday";
+    public string currentMonthOfYear = "January";
 
     private void Start()
     {
         if (currentDay == 0) currentDay = 1;
+        daysMoth[0].SetActive(false);
+        daysMoth[1].SetActive(true);
+        daysMoth[2].SetActive(false);
+        daysMoth[3].SetActive(false);
+        daysMoth[4].SetActive(false);
+        daysMoth[5].SetActive(false);
+        daysMoth[6].SetActive(false);
+        daysMoth[7].SetActive(false);
     }
 
     private void Update()
@@ -50,22 +61,23 @@ public class TimeSystem : MonoBehaviour
         minuteText.text = CurrentMinutes.ToString("00");
         dayText.text = "Day: " + currentDay;
         weekDayText.text = "Day of the week: " + _daysOfWeek[currentDay % 7];
+        monthText.text = _MonthOfYear[currentMonth % 12];//cosas del mes aun no funka
     }
 
     void WorldTime()
     {
+        # region horas
         if (currentHour >= 24f)
         {
             currentDay++;
             currentHour = 0;
             currentDayOfWeek = _daysOfWeek[currentDay % 7];
         }
-        if(CurrentMinutes>=60f)
+        if (CurrentMinutes>=60f)
         {
             currentHour++;
             CurrentMinutes = 0;
         }
-
         if(currentHour>=0 && currentHour<12)
         {
             AmPm.text = "am";
@@ -74,6 +86,216 @@ public class TimeSystem : MonoBehaviour
         {
             AmPm.text = "pm";
         }
+        #endregion
+        #region mes
+        switch (currentMonth)
+        {
+            case 0:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(true);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 1:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(true);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 2:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(true);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 3:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(true);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 4:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(true);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 5:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(true);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 6:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(true);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 7:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(true);
+                    break;
+                }
+            case 8:
+                {
+                    daysMoth[0].SetActive(true);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 9:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(true);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 10:
+                {
+                    daysMoth[0].SetActive(false);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(true);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+            case 11:
+                {
+                    daysMoth[0].SetActive(true);
+                    daysMoth[1].SetActive(false);
+                    daysMoth[2].SetActive(false);
+                    daysMoth[3].SetActive(false);
+                    daysMoth[4].SetActive(false);
+                    daysMoth[5].SetActive(false);
+                    daysMoth[6].SetActive(false);
+                    daysMoth[7].SetActive(false);
+                    break;
+                }
+        }
+        if (currentDay>31 && currentMonth==0)//enero
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 28 && currentMonth == 1)//febrero
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 30 && currentMonth == 2)//marzo
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 31 && currentMonth == 3)//abril
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 31 && currentMonth == 4)//mayo
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 29 && currentMonth == 5)//junio
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 31 && currentMonth == 6)//julio
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 31 && currentMonth == 7)//agosto
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 31 && currentMonth == 8)//septiembre
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 31 && currentMonth == 9)//octubre
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 30 && currentMonth == 10)//noviembre
+        {
+            currentMonth++;
+            currentDay = 1;
+        }
+        else if (currentDay > 31 && currentMonth == 11)//diciembre
+        {
+            currentMonth=0;
+            currentDay = 1;
+        }
+        #endregion
     }
 
     public void ChangeHour() 
@@ -86,6 +308,7 @@ public class TimeSystem : MonoBehaviour
         currentDay++;
         numday.timeday = 25200;
         currentHour = 7;
+        CurrentMinutes = 0;
         currentDayOfWeek = _daysOfWeek[currentDay % 7];
     }
 }
